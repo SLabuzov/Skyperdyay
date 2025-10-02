69ff90fb4a762a660f54099e1f88cf214b64f51a
Sergey Labuzov <s.labuzov.public@gmail.com>
2025-10-02 15:09:39 +0300

    [FRONTEND] "Просмотр фин категорий (новый дизайн)"

--- client/app/(account)/categories/page.tsx
+++ client/app/(account)/categories/page.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import Link from 'next/link';
-import { Plus } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { categoryApiService } from '@/services/category-api-service';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { CategoryInfo } from '@/lib/types';
-import CategoryInfoCard from '@/components/category/category-info-card';
+import {categoryApiService} from "@/services/category-api-service";
+import {Plus} from "lucide-react";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+import CategoryInfoCard from "@/components/category/category-info-card";
 
 export default async function CategoriesPage() {
   const categories = await categoryApiService.getCategories();
+  const incomeCategories = categories.filter(item => item.categoryType === 'INCOME');
+  const expenseCategories = categories.filter(item => item.categoryType === 'EXPENSE');
 
   return (
     <div className="flex flex-col flex-1 gap-4 px-8 overflow-hidden">
@@ -21,40 +20,18 @@ export default async function CategoriesPage() {
           </Link>
         </Button>
       </div>
-      <div className="grid grid-cols-2 gap-4 overflow-y-auto">
-        <Tabs defaultValue="ALL" className="mb-8">
-          <TabsList>
-            <TabsTrigger value="ALL">Все категории</TabsTrigger>
-            <TabsTrigger value="INCOME">Доходные категории</TabsTrigger>
-            <TabsTrigger value="EXPENSE">Расходные категории</TabsTrigger>
-          </TabsList>
-          <TabsContent value="ALL" className="mt-6">
-            <TabContent categories={ categories }
-            />
-          </TabsContent>
-          <TabsContent value="INCOME" className="mt-6">
-            <TabContent
-              categories={ categories.filter(item => item.categoryType === 'INCOME') }
-            />
-          </TabsContent>
-          <TabsContent value="EXPENSE" className="mt-6">
-            <TabContent
-              categories={ categories.filter(item => item.categoryType === 'EXPENSE') }
-            />
-          </TabsContent>
-        </Tabs>
+      <div className="grid grid-cols-2 gap-8 overflow-y-auto">
+        <div className="flex flex-col gap-4">
+          {incomeCategories.map(category => (
+            <CategoryInfoCard key={category.categoryId} categoryInfo={category}/>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4">
+          {expenseCategories.map(category => (
+            <CategoryInfoCard key={category.categoryId} categoryInfo={category}/>
+          ))}
+        </div>
       </div>
     </div>
   );
-}
-
-function TabContent({ categories }: { categories: CategoryInfo[] }) {
-
-  return (
-    <div className="flex flex-col gap-4">
-      { categories.map(category => (
-        <CategoryInfoCard key={ category.categoryId } categoryInfo={ category }/>
-      )) }
-    </div>
-  )
 }
--- client/components/category/category-info-card.tsx
+++ client/components/category/category-info-card.tsx
@@ -1,5 +1,6 @@
-import { CategoryInfo } from '@/lib/types';
-import { CategoryIcon } from '@/components/category/category-icon';
+import {CategoryInfo} from '@/lib/types';
+import {CategoryIcon} from '@/components/category/category-icon';
+import {TrendingDown, TrendingUp} from 'lucide-react';
 
 interface Props {
   categoryInfo: CategoryInfo
@@ -7,28 +8,26 @@ interface Props {
 
 export default function CategoryInfoCard({ categoryInfo }: Props) {
 
+  let descriptionCategoryType = 'доходная категория';
+  let trendingIcon = <TrendingUp size={12} className="text-icon-success"/>;
+
   if (categoryInfo.categoryType === 'EXPENSE') {
-    return (
-      <div className="flex items-center border border-[#E5E6E6] p-[14px] rounded-[16px] gap-[14px]">
-        <div className="flex items-center justify-center w-9 h-9 rounded-full bg-[#FDCED1] text-[#F73541]">
-          <CategoryIcon name={ categoryInfo.categoryIcon } size={ 20 }/>
-        </div>
-        <div className="flex flex-col">
-          <span className="font-medium text-gray-600">{ categoryInfo.categoryName }</span>
-          <span className="text-sm font-base text-gray-400">0% от общего расхода</span>
-        </div>
-      </div>
-    );
+    descriptionCategoryType = 'расходная категория';
+    trendingIcon = <TrendingDown size={12} className="text-icon-error"/>;
   }
 
   return (
     <div className="flex items-center border border-[#E5E6E6] p-[14px] rounded-[16px] gap-[14px]">
-      <div className="flex items-center justify-center w-9 h-9 rounded-full bg-[#ECF4E9] text-primary">
-        <CategoryIcon name={ categoryInfo.categoryIcon } size={ 20 }/>
+      <div
+        className="flex items-center justify-center w-9 h-9 rounded-full bg-primary-weak border-1 border-stroke-weak text-primary">
+        <CategoryIcon name={categoryInfo.categoryIcon} size={20}/>
       </div>
       <div className="flex flex-col">
-        <span className="font-medium text-gray-600">{ categoryInfo.categoryName }</span>
-        <span className="text-sm font-base text-gray-400">0% от общего дохода</span>
+        <span className="flex items-center gap-1 text-sm text-strong font-medium">
+          {categoryInfo.categoryName}
+          {trendingIcon}
+        </span>
+        <span className="text-xs font-base text-weak">{descriptionCategoryType}</span>
       </div>
     </div>
   );
