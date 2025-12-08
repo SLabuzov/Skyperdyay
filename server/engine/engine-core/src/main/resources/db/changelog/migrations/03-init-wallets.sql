9cd2f8e5bae49448800df0a06c783844df3608d9
Sergey Labuzov <s.labuzov.public@gmail.com>
2025-12-08 15:18:30 +0300

    Правки в схеме БД

--- server/engine/engine-core/src/main/resources/db/changelog/migrations/03-init-wallets.sql
+++ server/engine/engine-core/src/main/resources/db/changelog/migrations/03-init-wallets.sql
@@ -15,3 +15,22 @@ CREATE TABLE moneybox.wallets
         FOREIGN KEY (currency_code) REFERENCES moneybox.dict_currencies (code)
 );
 --rollback DROP TABLE moneybox.wallets;
+
+--changeset SergeyLabuzov:drop-wallets-constraint-wallets_name_key
+ALTER TABLE moneybox.wallets
+    DROP CONSTRAINT wallets_name_key;
+--rollback ALTER TABLE moneybox.wallets ADD CONSTRAINT wallets_name_key UNIQUE (name);
+
+--changeset SergeyLabuzov:add-wallets-constraint-wallet_owner_name__unique
+ALTER TABLE moneybox.wallets
+    ADD CONSTRAINT wallet_owner_name__unique UNIQUE ( owner, name );
+--rollback ALTER TABLE moneybox.wallets DROP CONSTRAINT wallet_owner_name__unique;
+
+--changeset SergeyLabuzov:add-wallets-constraint-wallet_owner_currency_code__unique
+ALTER TABLE moneybox.wallets
+    ADD CONSTRAINT wallet_owner_currency_code__unique UNIQUE ( owner, currency_code );
+--rollback ALTER TABLE moneybox.wallets DROP CONSTRAINT wallet_owner_currency_code__unique;
+
+--changeset SergeyLabuzov:add-wallets-index-idx_wallets_currencies__fk
+CREATE INDEX IF NOT EXISTS idx_wallets_currencies__fk ON moneybox.wallets ( currency_code );
+--rollback DROP INDEX IF EXISTS idx_wallets_currencies__fk;
